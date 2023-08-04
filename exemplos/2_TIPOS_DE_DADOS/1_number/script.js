f00af6f3b4dc1429640644cf5f0b987f530a8638
intellectualgreed <victor.orodrigues07senai@gmail.com>
2023-08-04 16:08:59 -0300

    .

---
+++ exemplos/2_TIPOS_DE_DADOS/1_number/script.js
@@ -0,0 +1,9 @@
+console.log(5);
+console.log(35.7);
+console.log(-76);
+console.log(1858.59);
+
+console.log(typeof 5);
+console.log(typeof 35.7);
+console.log(typeof -76);
+console.log(typeof 1858.59);
---
+++ exemplos/2_TIPOS_DE_DADOS/4_string/script.js
@@ -0,0 +1,9 @@
+console.log("Meu nome é Victor");
+console.log('Desenvolvimento é no Senai');
+
+// Template literals (crase) ao lado da tecla P
+// Usar shift
+console.log(`teste`);
+
+console.log(typeof "Infinity"); 
+console.log(typeof Infinity);
