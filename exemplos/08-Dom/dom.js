e52b8b3a747b53611a6656901ad0b757141a53dc
intellectualgreed <victor.orodrigues07senai@gmail.com>
2023-11-16 09:46:16 -0300

    j

--- exemplos/08-Dom/dom.js
+++ exemplos/08-Dom/dom.js
@@ -2,14 +2,14 @@
 
 
 // Encontre o elemento <img> pelo atributo alt
-const imgElement = document.querySelector('img[alt="DOM"]');
+// const imgElement = document.querySelector('img[alt="DOM"]');
 
 // Defina o caminho local da imagem
 // const caminhoLocal = 'img/A_Veloz.jpg';
-const caminhoLocal = 'img/B_Veloz.jpg';
+// const caminhoLocal = 'img/B_Veloz.jpg';
 
-// Altere o atributo src do elemento <img> para o caminho local
-imgElement.setAttribute('src', caminhoLocal);
+// // Altere o atributo src do elemento <img> para o caminho local
+// imgElement.setAttribute('src', caminhoLocal);
 
 // ____________________________________________________________________________
 
@@ -23,7 +23,13 @@ querySelectorAll() -> seleciona VÁRIOS elementos de acordo com um seletor */
 // Modificando elementos
 
 
-// 1ª Digitação (Aqui)
+const titulo = document.getElementById('titulo-principal');
+
+const pagina = document.querySelector('body'); 
+
+const subtitulo = document.querySelectorAll('h2');
+
+const legenda = document.querySelector('figcaption');
 
 
 // ____________________________________________________________________________
@@ -31,8 +37,13 @@ querySelectorAll() -> seleciona VÁRIOS elementos de acordo com um seletor */
 // Modificando elementos
 
 
-// 2ª Digitação (Aqui)
+titulo.textContent = "Olá mundo!";
 
+pagina.style.backgroundColor = "salmon";
+
+subtitulo[1].textContent="Full-Stack";
+
+legenda.innerHTML="<b>Veloz e furioso</b>";
 
 // ____________________________________________________________________________
 
@@ -49,11 +60,11 @@ const subtitulos = document.querySelectorAll('h2');
 // Modifique a cor da fonte de todos os elementos <h2> para azul, por exemplo
 
 // Sintaxe (Arrow Function)
-// subtitulos.forEach(subtitulo => {
-//   subtitulo.style.color = 'blue';
-// });
+subtitulos.forEach(subtitulo => {
+  subtitulo.style.color = 'blue';
+});
 
-// Sintaxe For...of (Loop comum)
+// // Sintaxe For...of (Loop comum)
 // for (const subtitulo of subtitulos) {
 //     subtitulo.style.color = 'blue';
 //   }
@@ -65,15 +76,25 @@ const subtitulos = document.querySelectorAll('h2');
 
 // Modificar o texto do <h3> "Editores"
 
+const tituloEditores = document.querySelector('h3');
 
-// 3ª Digitação (Aqui)
+tituloEditores.textContent = 'Melhores Editores';
+
+tituloEditores.style.color = 'Orange';
+
+tituloEditores.style.fontSize = '28px';
+
 
 
 // Adicionar um novo editor à lista:
 
+const listaEditores = document.querySelector('#lista-editores');
+const novoEditor = document.createElement('li');
 
-// 4ª Digitação (Aqui)
+novoEditor.textContent = 'Sublime';
+listaEditores.appendChild(novoEditor);
 
 
 
-
+const terceiroItem = listaEditores.querySelectorAll('li')[2];
+terceiroItem.style.color = 'gold'
